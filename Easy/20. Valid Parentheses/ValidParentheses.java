fab0231af3653e86a40e661343609188c7cee000
Shahjahan37 <91134923+Shahjahan37@users.noreply.github.com>
2023-07-30 16:53:41 +0530

    Java Code of ( Valid Parentheses )

---
+++ Easy/20. Valid Parentheses/ValidParentheses.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack();
+        Map<Character, Character> map = new HashMap();
+        map.put('(', ')');
+        map.put('[', ']');
+        map.put('{', '}');
+        
+        for(char c : s.toCharArray()){
+            if(map.containsKey(c))
+                st.push(c);
+            else if(st.isEmpty() || c != map.get(st.pop()))
+                return false;
+        }
+        return st.isEmpty();
+    }
+}
