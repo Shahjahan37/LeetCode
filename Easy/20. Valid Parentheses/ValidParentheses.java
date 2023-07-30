57774d56e5cbee02e64fcab4c154a94de01a2f18
Shahjahan37 <91134923+Shahjahan37@users.noreply.github.com>
2023-07-30 17:26:50 +0530

    File Updated

---
+++ Easy/20. Valid Parentheses/ValidParentheses.java
@@ -0,0 +1,18 @@
+class Solution {
+    // valid parentheses Java code
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack();
+        Map<Character, Character> map = new HashMap();
+        map.put('(', ')');
+        map.put('[', ']');
+        map.put('{', '}');
+        
+        for(char c : s.toCharArray()){
+            if(map.containsKey(c))
+                st.push(c);
+            else if(st.isEmpty() || c != map.get(st.pop()))
+                return false;
+        }
+        return st.isEmpty(); 
+    }
+}
